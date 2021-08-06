408bba45775eeb054e4ca129b7765e972791675b
zoniha <awaawapurin@gmail.com>
2021-08-06 12:21:44 +0900

    Initial Commit

---
+++ page_transition/ModalViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ModalViewController.swift
+//  page_transition
+//
+//  Created by zoniha on 2021/08/06.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+	@IBAction func backButton(_ sender: Any) {
+		dismiss(animated: true, completion: nil)
+	}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
